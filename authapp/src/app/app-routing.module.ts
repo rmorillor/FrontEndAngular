22efa911fb8ce8205f3edbcc38786939afd2abd2
Roy Morillo <rmorillor@gmail.com>
2021-09-01 11:10:41 -0400

    Ajsutes prod
    Ajustes para produccion

--- authapp/src/app/app-routing.module.ts
+++ authapp/src/app/app-routing.module.ts
@@ -29,7 +29,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false
+  })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
